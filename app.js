2c77f2cdf4dc3ea62e282ebbd51dbda042a2222d
dharadalal <dharadalalm@gmail.com>
2023-03-17 08:30:45 -0400

    Responsive navbar

--- app.js
+++ app.js
@@ -14,3 +14,37 @@ function bounce(letter) {
     }, 2000);
   }
 }
+
+// responsive menu
+const menuIcon = document.querySelector(".menu-icon");
+const navbarContainer = document.querySelector(".navbar-container");
+const navContent = document.querySelector(".nav-content");
+const menubarIcon = document.querySelector(".menu-icon>i");
+const navbarBackdrop = document.querySelector(".navbar-backdrop");
+
+// Drawer open
+menuIcon.addEventListener("click", () => {
+  if (
+    navbarContainer.classList.contains("hide") &&
+    navContent.classList.contains("hide")
+  ) {
+    navbarContainer.classList.remove("hide");
+    navContent.classList.remove("hide");
+    navContent.classList.add("show");
+    menubarIcon.classList.remove("fa-bars");
+    menubarIcon.classList.add("fa-close");
+  } else {
+    closeDrawer();
+  }
+});
+
+navbarBackdrop.addEventListener("click", closeDrawer);
+
+function closeDrawer() {
+  navContent.classList.remove("show");
+  navbarContainer.classList.add("hide");
+  navContent.classList.add("hide");
+
+  menubarIcon.classList.add("fa-bars");
+  menubarIcon.classList.remove("fa-close");
+}
